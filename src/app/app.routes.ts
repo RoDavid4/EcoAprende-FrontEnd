0c5ed9b14ac61ecbd9dc105a5267984944e7564c
Ale <ale-210@live.com>
2026-08-10 20:44:15 -0300

    feat:agrega guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { Home } from './features/home/home'
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,6 @@ export const routes: Routes = [
    {
     path: 'home',
     component: Home,
+    canActivate: [authGuard],
   },
 ];
