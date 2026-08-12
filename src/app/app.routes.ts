1bd89cf8b19a8468353487d86819f4e3dc5cddcf
paolaCampero11 <paolacampero32@gmail.com>
2026-08-12 00:57:20 -0400

    feat(routes): Configuracion de rutas y configuraciones adicionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TeacherDashboard } from './features/classrooms/pages/teacher-dashboard/teacher-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'classrooms/teacher',
+    pathMatch: 'full',
+  },
+  {
+    path: 'classrooms/teacher',
+    component: TeacherDashboard,
+  },
+
+  {
+    path: '**',
+    redirectTo: 'classrooms/teacher',
+  },
+];
