5bb8970707f8d0e7e6f65499c07fb18aef34287c
Ale <ale-210@live.com>
2026-08-19 23:42:17 -0300

    feat:agrega elementoshome

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,22 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AuthService } from '../auth/services/auth.services';
 
 @Component({
   selector: 'app-home',
   imports: [
-    MatButtonModule,
-    MatIconModule,
+     MatButtonModule,
+  MatIconModule,
+  RouterLink,
+  MatCardModule,
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  userName = '';
+  userRole = '';
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+
+      this.userName = user.fullName;
+      this.userRole = user.role;
+    }
+  }
 
   logout(): void {
     this.authService.logout();
